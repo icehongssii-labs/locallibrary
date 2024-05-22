7b782da19cce200caf185d2010a0b897f79fb96c
icehongssii12345 <icehongssii@gmail.com>
2024-05-23 01:21:32 +0900

    Feat: Add Book-list html

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+    # 저기에 있는 name 파라미터로
+    # <html><a href="{% url 'index' %}">Home</a>.</html>
+    # 이름식으로 url이름을 지어줄수잇음
+    # 그냥 하드코딩헤서 /catalog/index 이런식으로 할수잇지만
+path('', views.index, name='index'),
+# 이번에는 view를 클래스형식으로 가져올것임
+path('books/', views.BookListView.as_view(), name='books'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,26 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 # Create your views here.
+from django.views import generic
+
+class BookListView(generic.ListView):
+    # model=book으로 generic view가 db에서 book에잇는 레코드 전부가져올것임
+    model = Book
+    # 그리고 book_list.html에 렌더할것임 ({객체명}_list.html)
+    # generic view가 /{어플리케이션이름}/{모델명}_list.html을찾아다닐거임 /{어프리케이션/templates안에서
+    # 이런기본설정은 따로 바꿀수있긴함 만약에 특정짓고싶다면 아래와 같이
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return Book.objects.filter(title__icontains='live')[:5]
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        # model말고 추가로 더 렌더링하고싶은게잇다면
+        context =  super(BookListView, self).get_context_data(**kwargs)
+        context['some'] = '추가어쩌구 ㅋ'
+        return context
+
 
 def index(request):
     num_instances = BookInstance.objects.all().count()
@@ -22,3 +42,6 @@ def index(request):
     # html
     # html에 렌더링될 변수
     return render(request,'index.html',context)
+
+class BookDetailView(generic.DetailView):
+    model = Book
